4a2f9cdaa58162ca50c4260120f58b15fdea9b86
sujit gouda <sujit.gda997@gmail.com>
2024-12-08 12:02:32 +0530

    final udpate

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -24,7 +24,7 @@ const HomePage = () => {
           <h3 className="text-lg font-semibold mb-6 text-blue-600">
           Empowering Idea, Innovation and Impact
           </h3>
-          <p className="text-xl mb-8 max-w-2xl mx-auto font-light text-gray-600">
+          <p className="text-lg mb-8 max-w-3xl mx-auto font-light text-gray-600">
             Where Ideas Thrive, Innovation Ignites, and Impact Reshapes the Future.
           </p>
           {/* <button className="border-blue-500 border text-blue-500 hover:bg-blue-500 hover:text-white px-6 py-3 rounded-sm transition-all duration-300 transform hover:scale-105"> */}
